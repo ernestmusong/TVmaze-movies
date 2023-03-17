d59a4af9bdbd75daa924b26c8646721cb741041d
atok624 <84607674+atok624@users.noreply.github.com>
2023-03-17 21:59:15 +0000

    Update the project

--- test.test.js
+++ test.test.js
@@ -3,6 +3,8 @@
  */
 
 // TEST FOR ITEMS COUNTER
+import SHOWS from './src/home.js';
+
 const itemsArray = [];
 const mockedApiLength = 20;
 let count = 0; // count displays items count
@@ -46,5 +48,26 @@ describe('counters', () => {
   test('update comments count', () => {
     addComment();
     expect(commentsCount).toBe(1);
+  });
+});
+
+describe('SHOWS', () => {
+  describe('getItemCount', () => {
+    it('returns the correct number of items', () => {
+      const shows = [{}, {}, {}];
+      SHOWS.allShows = shows;
+      expect(SHOWS.getItemCount()).toEqual(3);
+    });
+  });
+
+  describe('getCommentCount', () => {
+    it('returns the correct number of comments', () => {
+      const shows = [{ comments: [{}, {}] },
+        { comments: [{}, {}, {}] },
+        { comments: [] },
+      ];
+      SHOWS.allShows = shows;
+      expect(SHOWS.getCommentCount()).toEqual(5);
+    });
   });
 });
