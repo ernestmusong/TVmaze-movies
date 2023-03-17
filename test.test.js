667c323381505ac576f1c76075f041c567254bd4
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-17 16:50:55 +0100

    fix linters

--- test.test.js
+++ test.test.js
@@ -3,21 +3,20 @@
  */
 
 // TEST FOR ITEMS COUNTER
-let itemsArray = [];
-let mockedApiLength = 20
-let count = 0 // count displays items count
+const itemsArray = [];
+const mockedApiLength = 20;
+let count = 0; // count displays items count
 const updateCount = (array) => {
-    for(let i = 0; i < mockedApiLength; i += 1){
-        array.push(i)
-        count = array.length
-    }
-    return array
-}
+  for (let i = 0; i < mockedApiLength; i += 1) {
+    array.push(i);
+    count = array.length;
+  }
+  return array;
+};
 
 describe('todo', () => {
   test('update items count', () => {
     updateCount(itemsArray);
     expect(count).toBe(20);
   });
-   
-  });
+});
